50ee75416f8f89e0507511508290b294e52266b2
Gabriel Penke <gabri.penke@gmail.com>
2023-12-07 23:09:43 -0300

    feat: integrate with accuWeather API

--- src/Search.js
+++ src/Search.js
@@ -4,20 +4,28 @@ const Search = () => {
 
     function searchInput(event) {
         let currentValue = document.querySelector('input[name="searchInput"]').value;
+
+        const urlAccuWeather = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=K3rgPRk6b2GzxJgChMYMtxdQHuuxTxAB&q=${currentValue}`;
+        fetch(urlAccuWeather)
+         .then(response => response.json())
+            .then(data => {
+                // const {main, name, sys, weather} = data;
+                console.log(data);
+            })
         /*
          Fazer requisição API depois
         */
-       const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&APPID=4b280f067015f08032ff03d7bb174c40`;
-       fetch(url)
-         .then(response => response.json())
-            .then(data => {
-                const {main, name, sys, weather} = data;
-                console.log(data);
-                if(weather !== undefined){
+    //    const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&APPID=4b280f067015f08032ff03d7bb174c40`;
+    //    fetch(url)
+    //      .then(response => response.json())
+    //         .then(data => {
+    //             const {main, name, sys, weather} = data;
+    //             console.log(data);
+    //             if(weather !== undefined){
 
-                    // console.log(weather[0]['description']);
-                }
-            })
+    //                 // console.log(weather[0]['description']);
+    //             }
+    //         })
     }
     return (
         <div className='search'>
