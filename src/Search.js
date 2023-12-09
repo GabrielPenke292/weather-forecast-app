feaa1eeb218ce8244f1577b36e031ef08ce29562
Gabriel Penke <gabri.penke@gmail.com>
2023-12-08 21:52:36 -0300

    feat: integrate both APIs in the system

--- src/Search.js
+++ src/Search.js
@@ -27,13 +27,30 @@ function Search() {
         }
     }
 
+    function getWeatherForecast(city, country){
+        
+        setShowSuggestions(false)
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=4b280f067015f08032ff03d7bb174c40`;
+
+        fetch(url)
+         .then(response => response.json())
+            .then(data => {
+                const {main, name, sys, weather} = data;
+                console.log(data);
+                if(weather !== undefined){
+
+                    console.log(weather[0]['description']);
+                }
+            })
+    }
+
     return (
         <div className='search'>
             <input type='text' name='searchInput' onKeyUp={searchInput} placeholder='Search City...' />
             {showSuggestions && (
                 <div className="suggestions">
                     {cities.map((city, index) => (
-                        <button key={index} onClick={() => console.log(city.Key)}>
+                        <button key={index}  onClick={()=>getWeatherForecast(city.LocalizedName, city.Country.ID)}>
                             {city.LocalizedName}
                         </button>
                     ))}
